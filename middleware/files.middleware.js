006492de3c520b02d35df418b20dd6161c57b0d3
Mosiur411 <mosiurislamwebdesign@gmail.com>
2023-05-18 23:47:44 +0600

    insoallha hoye jabe

--- middleware/files.middleware.js
+++ middleware/files.middleware.js
@@ -9,7 +9,7 @@ const { getRandomString } = require('../utils/helpers')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads")
+    cb(null, "../uploads")
   },
   filename: function (req, file, cb) {
     const newFIleName = `${getRandomString()}-${file.originalname}`
