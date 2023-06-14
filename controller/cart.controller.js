85158d37a8afefd87e0cf367c225e665ba01f310
Mosiur411 <mosiurislamwebdesign@gmail.com>
2023-06-15 05:52:05 +0600

    order add problem solve insoallha

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -45,7 +45,7 @@ const addCart = async (req, res) => {
         } else {
             const qut = Number(checkProuct[0]?.quantity) + Number(data?.quantity);
             const productPrice = checkProuct[0]?.saleing_Price / Number(checkProuct[0]?.quantity)
-            //await ProductModel.findOneAndUpdate({ _id: product_id }, { quantity: handelProuct?.quantity - data?.quantity }, { new: true })
+            await ProductModel.findOneAndUpdate({ _id: product_id }, { quantity: handelProuct?.quantity - data?.quantity }, { new: true })
             addToCar = await CartModel.findOneAndUpdate({ product_id }, { quantity: qut, saleing_Price: productPrice * qut }, { new: true })
         }
         return res.status(200).json({ addToCar })
